f4e0652c52fa811cd23c663720d2813b0be1a56f
Hugo Tavares <hm533@cam.ac.uk>
2025-07-08 14:45:03 +0100

    add samplesheets

---
+++ prepare_data/bulk_mononen2024/scripts/02-files_to_keep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This was run interactively, but included here for reference.
+# the paths may not be correct for your system.
+
+mv sample_info.csv nextflow_samplesheet.csv ~/Course_Materials/mononen2024/
+mv reads ~/Course_Materials/mononen2024/
+mv references ~/Course_Materials/mononen2024/
+mv results/rnaseq/start_salmon/tx2gene.tsv ~/Course_Materials/mononen2024/references/
+mv results/rnaseq/multiqc ~/Course_Materials/mononen2024/preprocessed/rnaseq/
+mv results/rnaseq/start_salmon/ ~/Course_Materials/mononen2024/preprocessed/rnaseq/
